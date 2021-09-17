21a27145894a9b743fbf6802854db7073823220b
Patrick 'FroskyArr' Chen <me@frost.moe>
2021-09-17 13:47:37 +0800

    feat: add tooltip for event and talent

--- src/CondExpr.svelte
+++ src/CondExpr.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { CondExpr } from "./utils/vendor.types";
   import * as vendor from "./utils/vendor"
+import App from "./App.svelte";
 
   export let expr: CondExpr
   export let depth = 0
@@ -20,7 +21,16 @@
   }
 
   function showIsNegate(val: string) {
-    return val === '!' ? '无' : ''
+    return val === '!' ? '未' : ''
+  }
+
+  function lookupRefCond(prop: string, id: number): string {
+    switch(prop) {
+      case 'TLT':  return vendor.TALENTS[id].name
+
+      case 'EVT':
+      case 'AEVT': return vendor.EVENTS[id].event
+    }
   }
 </script>
 
@@ -30,17 +40,29 @@
   </span>
 {:else if expr.tag === 'Ref'}
   <span class='nested-cond-{depth}'>
-    {#if expr.val[0] === 'TLT'}
-      {showIsNegate(expr.val[0])}天赋{expr.val[2].map(x => "#"+x)}
+
+    {showIsNegate(expr.val[1])
+    }{#if expr.val[0] === 'TLT'}
+      {expr.val[2].length > 1 ? '拥有下列任一天赋' : '拥有天赋'}
     {:else if expr.val[0] === 'EVT'}
-      {showIsNegate(expr.val[0])}事件{expr.val[2].map(x => "#"+x)}
+      {expr.val[2].length > 1 ? '曾发生下列任一事件' : `曾发生事件`}
     {:else if expr.val[0] === 'AEVT'}
-      {showIsNegate(expr.val[0])}继承事件{expr.val[2].map(x => "#"+x)}
+      {expr.val[2].length > 1 ? '此生或此生前曾发生下列任一事件'
+                              : `此生或此生前曾发生事件`}
     {/if}
+
+    {#each expr.val[2] as refId, index}
+      {#if index != 0},{/if}
+      <span class='tooltip'>
+        #{refId}
+        <span class='tooltiptext'>「{lookupRefCond(expr.val[0], refId)}」</span>
+      </span>
+    {/each}
+
   </span>
 {:else if expr.tag === 'Or'}
   <span class='nested-cond-{depth}'>
-    下列条件任一：
+    满足下列条件任一：
     {#each expr.val as subExpr, index}
       {#if index != 0}，{/if}
       <svelte:self expr={subExpr} depth={depth+1}></svelte:self>
@@ -48,9 +70,9 @@
   </span>
 {:else if expr.tag === 'And'}
   <span class='nested-cond-{depth}'>
-    下列所有条件：
     {#each expr.val as subExpr, index}
-      {#if index != 0}，{/if}
+      {#if index != 0}
+      {' '}且{/if}
       <svelte:self expr={subExpr} depth={depth+1}></svelte:self>
     {/each}
   </span>
@@ -61,12 +83,36 @@
 
 <style>
   .nested-cond-1 {
-    text-decoration-line: underline;
-    text-decoration-style: solid;
+    /* text-decoration-line: underline;
+    text-decoration-style: solid; */
   }
 
   .nested-cond-2 {
-    text-decoration-line: underline;
-    text-decoration-style: double;
+    /* text-decoration-line: underline;
+    text-decoration-style: double; */
+  }
+
+  .tooltip {
+    position: relative;
+    display: inline-block;
+    border-bottom: 1px dotted black;
+  }
+
+  .tooltip .tooltiptext {
+    visibility: hidden;
+    width: max-content;
+    background-color: black;
+    color: #fff;
+    text-align: center;
+    border-radius: 6px;
+    padding: 6px;
+    position: absolute;
+    z-index: 1;
+    bottom: 100%;
+    left: 20%;
+  }
+
+  .tooltip:hover .tooltiptext {
+    visibility: visible;
   }
 </style>
