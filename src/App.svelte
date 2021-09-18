ebd5dc16cb4eab946b94bc78f96189544ad4835e
Patrick 'FroskyArr' Chen <me@frost.moe>
2021-09-18 15:56:06 +0800

    style: reposition content

--- src/App.svelte
+++ src/App.svelte
@@ -139,74 +139,83 @@
 
   {#if _selected}
     <hr />
-    <h3>这个事件...</h3>
-    <ul>
-      {#if prerequisite}
-        <li>发生的前置条件：<Cond expr={prerequisite} /></li>
-      {/if}
-      {#if conflict}
-        <li>不会在满足下列条件时发生：<Cond expr={conflict} /></li>
-      {/if}
-    </ul>
-    <ul></ul>
-    <ul>
-      {#if factors.length > 0}
-        {#each factors.filter((x) => x.event) as factor}
-          <li>
-            若满足<Cond expr={factor.condition}
-            />，将于 「{vendor.EVENTS[factor.event].event}」 (<a
-              href={`/e/${factor.event}`}
-              use:link>#{factor.event}</a
-            >) 之后发生。
-          </li>
-        {/each}
-        {#if factors.filter((x) => x.age != undefined).length > 0}
-          <li>
-            可能在以下年龄自然发生：{numRangeToStr(
-              dedup(factors.filter((x) => x.age != undefined).map((x) => x.age))
-            )}
-          </li>
+    <div class='content'>
+      <h3>这个事件...</h3>
+      <ul>
+        {#if prerequisite}
+          <li>发生的前置条件：<Cond expr={prerequisite} /></li>
         {/if}
-      {:else}
-        <li>似乎不会发生。</li>
-      {/if}
-    </ul>
+        {#if conflict}
+          <li>不会在满足下列条件时发生：<Cond expr={conflict} /></li>
+        {/if}
+      </ul>
+      <ul></ul>
+      <ul>
+        {#if factors.length > 0}
+          {#each factors.filter((x) => x.event) as factor}
+            <li>
+              若满足<Cond expr={factor.condition}
+              />，将于 「{vendor.EVENTS[factor.event].event}」 (<a
+                href={`/e/${factor.event}`}
+                use:link>#{factor.event}</a
+              >) 之后发生。
+            </li>
+          {/each}
+          {#if factors.filter((x) => x.age != undefined).length > 0}
+            <li>
+              可能在以下年龄自然发生：{numRangeToStr(
+                dedup(factors.filter((x) => x.age != undefined).map((x) => x.age))
+              )}
+            </li>
+          {/if}
+        {:else}
+          <li>似乎不会发生。</li>
+        {/if}
+      </ul>
+    </div>
   {/if}
 
   {#if hierarchy.length > 0}
     <hr />
-    <h3>来自世界之神的引导...</h3>
-    <ul>
-      {#each hierarchy as evt}
-        <li>
-          [前 {evt.level} 个事件] 「{vendor.EVENTS[evt.event.event].event}」 (<a
-            href={`/e/${evt.event.event}`}
-            use:link>#{evt.event.event}</a
-          >)
-        </li>
-      {/each}
-    </ul>
+    <div class="content">
+      <h3>来自世界之神的引导...</h3>
+      <ul>
+        {#each hierarchy as evt}
+          <li>
+            [前 {evt.level} 个事件] 「{vendor.EVENTS[evt.event.event].event}」 (<a
+              href={`/e/${evt.event.event}`}
+              use:link>#{evt.event.event}</a
+            >)
+          </li>
+        {/each}
+      </ul>
+    </div>
   {/if}
 
   {#if hierarchyRoot.length > 0}
-    最久远的引导可能在这些年龄发生： {numRangeToStr(hierarchyRoot)}
+    <div class="content">
+      最久远的引导可能在这些年龄发生： {numRangeToStr(hierarchyRoot)}
+    </div>
   {/if}
 
   {#if laterEvents.length}
     <hr />
-    <h3>这个事件之后可能会...</h3>
-    <ul>
-      {#each laterEvents as [evtCond, evtId]}
-        <li>
-          若满足<Cond expr={evtCond} />，将发生「{vendor.EVENTS[evtId].event}」 (<a
-            href={`/e/${evtId}`}
-            use:link>#{evtId}</a
-          >)。
-        </li>
-      {/each}
-    </ul>
+    <div class='content'>
+      <h3>这个事件之后可能会...</h3>
+      <ul>
+        {#each laterEvents as [evtCond, evtId]}
+          <li>
+            若满足<Cond expr={evtCond} />，将发生「{vendor.EVENTS[evtId].event}」 (<a
+              href={`/e/${evtId}`}
+              use:link>#{evtId}</a
+            >)。
+          </li>
+        {/each}
+      </ul>
+    </div>
   {/if}
 </div>
+
 <hr />
 <div class="center">
   <a href="https://github.com/outloudvi/liferestart-godmode">本页源码</a> |
@@ -226,6 +235,13 @@
 
   #selector {
     width: 65vw;
+  }
+
+  .content {
+    max-width: 1000px;
+    display: block;
+    margin: auto;
+    text-align: left;
   }
 
   .App p {
